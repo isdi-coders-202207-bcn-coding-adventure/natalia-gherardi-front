bda68357aa6272634353c1d50516eb239dbf7d24
Natalia <natirgh@gmail.com>
2022-08-22 15:54:03 +0200

    changed heading className

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -4,25 +4,25 @@ const Counter = (): JSX.Element => {
       <li>
         <div className="counter__time">
           02
-          <h2 className="counter__title">days</h2>
+          <h2 className="counter__description">days</h2>
         </div>
       </li>
       <li>
         <div className="counter__time">
           20
-          <h2 className="counter__title">hours</h2>
+          <h2 className="counter__description">hours</h2>
         </div>
       </li>
       <li>
         <div className="counter__time">
           34
-          <h2 className="counter__title">minutes</h2>
+          <h2 className="counter__description">minutes</h2>
         </div>
       </li>
       <li>
         <div className="counter__time">
           09
-          <h2 className="counter__title">seconds</h2>
+          <h2 className="counter__description">seconds</h2>
         </div>
       </li>
     </ul>
