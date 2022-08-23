fdb1739176220bd29bcef298b1db5c4d17ed00f7
Natalia <natirgh@gmail.com>
2022-08-23 13:29:28 +0200

    styled counter

---
+++ src/components/Counter/ConterStyled.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const CounterStyled = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  padding: 0;
+  .counter {
+    &__item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    &__description {
+      color: #ebedec;
+      font-size: 0.9rem;
+    }
+    &__time {
+      padding: 16px 26px;
+      background-color: #ffc700;
+      border-radius: 10px;
+      width: 20px;
+      color: #243342;
+      font-size: 2rem;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
+
+export const StyledLi = styled.li``;
+
+export const StyledNumberDiv = styled.div``;
+
+export const StyledWord = styled.span``;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import "@fontsource/roboto";
 import { store } from "./store/store";
 import App from "./App";
 import "./index.css";
+import "@fontsource/oxanium";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
